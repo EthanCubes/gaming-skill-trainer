010a5901e354a4909b55df13f35b734634bdf3cb
EthanCubes <ethanshangyilin@protonmail.com>
2026-07-28 13:44:46 -0600

    Added better pathfinding.

--- src/js/script.js
+++ src/js/script.js
@@ -1,16 +1,18 @@
-cps_button = document.getElementById("cps_test")
-cps_button.addEventListener("click", () => redirect("../pages/cps.html")) // The () => is AI because I don't know how to code apparently.
+let link = console.log(window.location.href);
 
-aim_trainer_button = document.getElementById("aim_trainer")
-aim_trainer_button.addEventListener("click", () => redirect("../pages/aim_trainer.html"))
+cps_button = document.getElementById("cps_test");
+cps_button.addEventListener("click", () => redirect("src/pages/cps.html")); // The () => is AI because I don't know how to code apparently.
 
-alt_spam_button = document.getElementById("alt_spam_test")
-alt_spam_button.addEventListener("click", () => redirect("../pages/alt_spam.html"))
+aim_trainer_button = document.getElementById("aim_trainer");
+aim_trainer_button.addEventListener("click", () => redirect("src/pages/aim_trainer.html"));
 
-typing_speed_button = document.getElementById("typing_speed_test")
-typing_speed_button.addEventListener("click", () => redirect("../pages/typing_speed.html"))
+alt_spam_button = document.getElementById("alt_spam_test");
+alt_spam_button.addEventListener("click", () => redirect("src/pages/alt_spam.html"));
 
-function redirect(link) {
-    window.open(link, "_self")
+typing_speed_button = document.getElementById("typing_speed_test");
+typing_speed_button.addEventListener("click", () => redirect("src/pages/typing_speed.html"));
+
+function redirect(page) {
+    window.open((link+page), "_self")
     console.log("hi")
 }
