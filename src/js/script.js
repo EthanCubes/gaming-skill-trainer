5ad1655c8fd9946e7a88b47339b26713d63a1c7b
EthanCubes <ethanshangyilin@protonmail.com>
2026-07-28 13:47:34 -0600

    Reversed previous change because turns out the slash was already there.

--- src/js/script.js
+++ src/js/script.js
@@ -1,16 +1,16 @@
 let link = console.log(window.location.href);
 
 cps_button = document.getElementById("cps_test");
-cps_button.addEventListener("click", () => redirect("/src/pages/cps.html")); // The () => is AI because I don't know how to code apparently.
+cps_button.addEventListener("click", () => redirect("src/pages/cps.html")); // The () => is AI because I don't know how to code apparently.
 
 aim_trainer_button = document.getElementById("aim_trainer");
-aim_trainer_button.addEventListener("click", () => redirect("/src/pages/aim_trainer.html"));
+aim_trainer_button.addEventListener("click", () => redirect("src/pages/aim_trainer.html"));
 
 alt_spam_button = document.getElementById("alt_spam_test");
-alt_spam_button.addEventListener("click", () => redirect("/src/pages/alt_spam.html"));
+alt_spam_button.addEventListener("click", () => redirect("src/pages/alt_spam.html"));
 
 typing_speed_button = document.getElementById("typing_speed_test");
-typing_speed_button.addEventListener("click", () => redirect("/src/pages/typing_speed.html"));
+typing_speed_button.addEventListener("click", () => redirect("src/pages/typing_speed.html"));
 
 function redirect(page) {
     window.open((link+page), "_self")
