47d168c34331ef393d4f53c2a355388b4d36f835
EthanCubes <ethanshangyilin@protonmail.com>
2026-07-28 13:30:05 -0600

    Added function for redirection by the fake buttons.

---
+++ src/js/script.js
@@ -0,0 +1,16 @@
+cps_button = document.getElementById("cps_test")
+cps_button.addEventListener("click", () => redirect("../pages/cps.html")) // The () => is AI because I don't know how to code apparently.
+
+aim_trainer_button = document.getElementById("aim_trainer")
+aim_trainer_button.addEventListener("click", () => redirect("../pages/aim_trainer.html"))
+
+alt_spam_button = document.getElementById("alt_spam_test")
+alt_spam_button.addEventListener("click", () => redirect("../pages/alt_spam.html"))
+
+typing_speed_button = document.getElementById("typing_speed_test")
+typing_speed_button.addEventListener("click", () => redirect("../pages/typing_speed.html"))
+
+function redirect(link) {
+    window.open(link, "_self")
+    console.log("hi")
+}
