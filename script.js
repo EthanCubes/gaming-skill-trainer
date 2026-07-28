dab1d9254a1f9392f424897f1eca3851e5647257
EthanCubes <ethanshangyilin@protonmail.com>
2026-07-28 14:44:18 -0600

    Fixed redirection to work.

--- script.js
+++ script.js
@@ -2,16 +2,16 @@ let link = window.location.href;
 console.log(link);
 
 cps_button = document.getElementById("cps_test");
-cps_button.addEventListener("click", () => redirect("src/pages/cps.html")); // The () => is AI because I don't know how to code apparently.
+cps_button.addEventListener("click", () => redirect("cps.html")); // The () => is AI because I don't know how to code apparently.
 
 aim_trainer_button = document.getElementById("aim_trainer");
-aim_trainer_button.addEventListener("click", () => redirect("src/pages/aim_trainer.html"));
+aim_trainer_button.addEventListener("click", () => redirect("aim_trainer.html"));
 
 alt_spam_button = document.getElementById("alt_spam_test");
-alt_spam_button.addEventListener("click", () => redirect("src/pages/alt_spam.html"));
+alt_spam_button.addEventListener("click", () => redirect("alt_spam.html"));
 
 typing_speed_button = document.getElementById("typing_speed_test");
-typing_speed_button.addEventListener("click", () => redirect("src/pages/typing_speed.html"));
+typing_speed_button.addEventListener("click", () => redirect("typing_speed.html"));
 
 function redirect(page) {
     url = link + page;
