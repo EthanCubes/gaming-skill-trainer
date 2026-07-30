e2fd790fc7e1415919d738ddeab51b1c0526a182
EthanCubes <ethanshangyilin@protonmail.com>
2026-07-30 10:56:35 -0600

    Added function to move keys around. Does not render anything yet though.

--- focus.js
+++ focus.js
@@ -7,22 +7,27 @@ const key6 = document.getElementById("key6");
 const key7 = document.getElementById("key7");
 const key8 = document.getElementById("key8");
 
+const position_position_index = [[80, 20], [90, 20], [80, 40], [90, 40], [80, 60], [90], [60], [80, 80], [90, 80]];
 let position_list = [key1, key2, key3, key4, key5, key6, key7, key8];
 const default_positions = [key1, key2, key3, key4, key5, key6, key7, key8];
+let desired_position;
+let time_offset = 0;
+
+let key_move_count = 0;
+
+let move_keys;
+let render_move_keys;
 
 function position_keys() {
     index = 0;
     for (const element of position_list) {
         index += 1;
         if (Math.round(index/2) === (index/2)) {
-            console.log(element);
             element.style.left = "80%";
         }
         else {
-            console.log(element);
             element.style.left = "90%";
         }
-        console.log(index);
         switch(index) {
             case 1:
             case 2:
@@ -194,6 +199,58 @@ function key_rotation() {
     temporary_list[6] = position_list[1];
     temporary_list[7] = position_list[0];
     return temporary_list;
+}
+
+function render_key_movement(target_position) {
+    time_offset += 10;
+    if (time_offset > 240) {
+        clearInterval(render_move_keys);
+        render_move_keys = null;
+        return;
+    }
+    for (const item in target_position) {
+        let current_position = position_list.indexOf(item);
+        console.log(position_list);
+        console.log(current_position);
+    }
+}
+
+function move_keys() {
+    key_move_count += 1;
+    if (key_move_count === 1) {
+        move_keys = setInterval(move_keys, 250);
+    }
+    if (key_move_count > 26) {
+        clearInterval(moveKeys);
+        move_keys = null;
+        key_move_count = 0;
+    }
+    if (key_move_count === 6) {
+        block_swap();
+    }
+    else {
+        if ((key_move_count === 10) || (key_move_count)) {
+            key_rotation();
+        }
+        else {
+            let random = Math.round(Math.random()*6);
+            switch(random) {
+                case 1:
+                    desired_position = small_rotation();
+                case 2:
+                    desired_position = big_rotation();
+                case 3:
+                    desired_position = shuffle();
+                case 4:
+                    desired_position = top_bottom_swap();
+                case 5:
+                    desired_position = swap();
+                case 6:
+                    desired_position = diagonal_swap();
+            }
+            render_move_keys = setInterval(render_key_movement, 10, desired_position)
+        }
+    }
 }
 
 function deepcopy(list) {
