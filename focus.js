d5bb45d99e313b22d76b41460c72e9c835316eca
EthanCubes <ethanshangyilin@protonmail.com>
2026-07-30 09:16:28 -0600

    Added dots to track and movement patterns that tells the program what the ending destinination is.

---
+++ focus.js
@@ -0,0 +1,205 @@
+const key1 = document.getElementById("key1");
+const key2 = document.getElementById("key2");
+const key3 = document.getElementById("key3");
+const key4 = document.getElementById("key4");
+const key5 = document.getElementById("key5");
+const key6 = document.getElementById("key6");
+const key7 = document.getElementById("key7");
+const key8 = document.getElementById("key8");
+
+let position_list = [key1, key2, key3, key4, key5, key6, key7, key8];
+const default_positions = [key1, key2, key3, key4, key5, key6, key7, key8];
+
+function position_keys() {
+    index = 0;
+    for (const element of position_list) {
+        index += 1;
+        if (Math.round(index/2) === (index/2)) {
+            console.log(element);
+            element.style.left = "80%";
+        }
+        else {
+            console.log(element);
+            element.style.left = "90%";
+        }
+        console.log(index);
+        switch(index) {
+            case 1:
+            case 2:
+                element.style.top = "20%";
+                break;
+            case 3:
+            case 4:
+                element.style.top = "40%";
+                break;
+            case 5:
+            case 6:
+                element.style.top = "60%";
+                break;
+            case 7:
+            case 8:
+                element.style.top = "80%";
+                break;
+        }
+    }
+}
+
+position_keys();
+
+// Movement 
+function small_rotation() {
+    let temporary_list = deepcopy(position_list);
+    if (Math.random() < 0.5) {
+        temporary_list[0] = position_list[2];
+        temporary_list[1] = position_list[0];
+        temporary_list[2] = position_list[3];
+        temporary_list[3] = position_list[1];
+    }
+    else {
+        temporary_list[0] = position_list[1];
+        temporary_list[1] = position_list[3];
+        temporary_list[2] = position_list[0];
+        temporary_list[3] = position_list[2];
+    }
+    if (Math.random() < 0.5) {
+        temporary_list[4] = position_list[6];
+        temporary_list[5] = position_list[4];
+        temporary_list[6] = position_list[7];
+        temporary_list[7] = position_list[5];
+    }
+    else {
+        temporary_list[4] = position_list[5];
+        temporary_list[5] = position_list[7];
+        temporary_list[6] = position_list[4];
+        temporary_list[7] = position_list[6];
+    }
+    return temporary_list;
+}
+function big_rotation() {
+    let temporary_list = deepcopy(position_list);
+    if (Math.random() < 0.5) {
+        temporary_list[0] = position_list[2];
+        temporary_list[1] = position_list[0];
+        temporary_list[2] = position_list[4];
+        temporary_list[3] = position_list[1];
+        temporary_list[4] = position_list[6];
+        temporary_list[5] = position_list[3];
+        temporary_list[6] = position_list[7];
+        temporary_list[7] = position_list[5];
+    }
+    else {
+        temporary_list[0] = position_list[1];
+        temporary_list[1] = position_list[3];
+        temporary_list[2] = posiiton_list[0];
+        temporary_list[3] = position_list[5];
+        temporary_list[4] = position_list[2];
+        temporary_list[5] = position_list[7];
+        temporary_list[6] = position_list[4];
+        temporary_list[7] = position_list[6];
+    }
+    return temporary_list;
+}
+function shuffle() {
+    let temporary_list = deepcopy(position_list);
+    if (Math.random() < 0.5) {
+        temporary_list[0] = position_list[0];
+        temporary_list[1] = position_list[2];
+        temporary_list[2] = posiiton_list[1];
+        temporary_list[3] = position_list[4];
+        temporary_list[4] = position_list[3];
+        temporary_list[5] = position_list[6];
+        temporary_list[6] = position_list[5];
+        temporary_list[7] = position_list[7];
+    }
+    else {
+        temporary_list[0] = position_list[3];
+        temporary_list[1] = position_list[1];
+        temporary_list[2] = posiiton_list[5];
+        temporary_list[3] = position_list[0];
+        temporary_list[4] = position_list[7];
+        temporary_list[5] = position_list[2];
+        temporary_list[6] = position_list[6];
+        temporary_list[7] = position_list[4];
+    }
+    return temporary_list;
+}
+function top_bottom_swap() {
+    let temporary_list = deepcopy(position_list);
+    if (Math.random() < 0.5) {
+        temporary_list[0] = position_list[2];
+        temporary_list[1] = position_list[3];
+        temporary_list[2] = posiiton_list[4];
+        temporary_list[3] = position_list[5];
+        temporary_list[4] = position_list[6];
+        temporary_list[5] = position_list[7];
+        temporary_list[6] = position_list[0];
+        temporary_list[7] = position_list[1];
+    }
+    else {
+        temporary_list[0] = position_list[6];
+        temporary_list[1] = position_list[7];
+        temporary_list[2] = posiiton_list[0];
+        temporary_list[3] = position_list[1];
+        temporary_list[4] = position_list[2];
+        temporary_list[5] = position_list[3];
+        temporary_list[6] = position_list[4];
+        temporary_list[7] = position_list[5];
+    }
+    return temporary_list;
+}
+function swap() {
+    let temporary_list = deepcopy(position_list);
+    temporary_list[0] = position_list[1];
+    temporary_list[1] = position_list[0];
+    temporary_list[2] = posiiton_list[3];
+    temporary_list[3] = position_list[2];
+    temporary_list[4] = position_list[5];
+    temporary_list[5] = position_list[4];
+    temporary_list[6] = position_list[7];
+    temporary_list[7] = position_list[6];
+    return temporary_list;
+}
+function diagonal_swap() {
+    let temporary_list = deepcopy(position_list);
+    temporary_list[0] = position_list[3];
+    temporary_list[1] = position_list[2];
+    temporary_list[2] = posiiton_list[1];
+    temporary_list[3] = position_list[0];
+    temporary_list[4] = position_list[7];
+    temporary_list[5] = position_list[6];
+    temporary_list[6] = position_list[5];
+    temporary_list[7] = position_list[4];
+    return temporary_list;
+}
+function block_swap() {
+    let temporary_list = deepcopy(position_list);
+    temporary_list[0] = position_list[4];
+    temporary_list[1] = position_list[5];
+    temporary_list[2] = posiiton_list[6];
+    temporary_list[3] = position_list[7];
+    temporary_list[4] = position_list[1];
+    temporary_list[5] = position_list[2];
+    temporary_list[6] = position_list[3];
+    temporary_list[7] = position_list[4];
+    return temporary_list;
+}
+function key_rotation() {
+    let temporary_list = deepcopy(position_list);
+    temporary_list[0] = position_list[7];
+    temporary_list[1] = position_list[6];
+    temporary_list[2] = posiiton_list[5];
+    temporary_list[3] = position_list[4];
+    temporary_list[4] = position_list[3];
+    temporary_list[5] = position_list[2];
+    temporary_list[6] = position_list[1];
+    temporary_list[7] = position_list[0];
+    return temporary_list;
+}
+
+function deepcopy(list) {
+    let copy = [];
+    for (const item in list) {
+        copy.push(item);
+    }
+    return copy;
+}
