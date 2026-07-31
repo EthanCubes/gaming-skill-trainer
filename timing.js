34bebe430ea4b0fda60b559aa7266b1a68fcb630
EthanCubes <ethanshangyilin@protonmail.com>
2026-07-31 12:33:45 -0600

    Removed code to get a fresh start again.

--- timing.js
+++ timing.js
@@ -1,26 +1,16 @@
-let testing = false;
-let interval;
-let start_time;
-let end_time = Date.now();
+/*
+# Game workflow
+First, the program starts off idle
+When a button is clicked, the test starts.
 
-document.addEventListener("click", mouse_clicked())
+When the test is started, every once in a while, a dot will try to move across the screen. 
+You have to click the mouse just as the dot hits the line.
+The test goes on for 20 seconds. After the test is done, the program resets back to what it used to be.
 
-function mouse_clicked() {
-    if (testing) {
-        if ((Date.now() - start_time) > 15000) {
-            testing = false;
-            end_time = Date.now();
-            clearInterval(interval);
-            interval = null;
-        }
-    }
-    else {
-        if ((Date.now() - end_time) > 500) {
-            testing = true;
-            interval = setInterval(gameloop(), 10);
-            start_time = Date.now();
-        }
-    }
-}
+If you click the circle just as it's on the line, you gain 300 points (like osu!). 
+And then, you only get 100 points, then you only get 50 points, then you don't get any points.
 
-function gameloop() {}
+At the end, something is broadcast to the user telling them how many points they got out of the maximum points they could have gotten.
+
+Now, how the hell do I do this?
+*/
