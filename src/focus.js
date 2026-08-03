634437e14a9684a305c48d2ba0c95e9bc055d30c
EthanCubes <ethanshangyilin@protonmail.com>
2026-08-03 15:22:26 -0600

    Added missing return statements that actually provide information.

--- src/focus.js
+++ src/focus.js
@@ -77,12 +77,14 @@ function get_key_pos(start_pos, end_pos, delay, duration) {
         // how to deal with vertical slope
         x_pos = start_pos[0];
         y_pos = -1 * (start_pos[1] - end_pos[1]) * delay / duration;
+        return [x_pos, y_pos];
     }
     slope = (start_pos[1] - end_pos[1]) / (start_pos[0] - end_pos[0]);
     x_pos = (start_pos[0] - end_pos[0]) * delay / duration;
     y_pos = x_pos * slope;
     x_pos += start_pos[0];
     y_pos += start_pos[1];
+    return [x_pos, y_pos];
 }
 
 // Determining the final position of the keys
