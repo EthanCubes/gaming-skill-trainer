4bc4ba4c872c31ee2e32f11fc2fe6f7c84642860
EthanCubes <ethanshangyilin@protonmail.com>
2026-08-05 21:15:14 -0600

    Fixed multiple different strings being generated upon program start because JavaScript is pretty fast and tries to load and process the files like 20 times before the files are actually loaded.

--- src/typing.js
+++ src/typing.js
@@ -1,7 +1,9 @@
 let testing_time = 15; // In this specific case, this refers to the time in seconds. But I really should make it consistent so that anyone reading the horrible code of mine does not get more confused.
 
 let data_fetched = false;
+let data_fetching = false;
 let words;
+let word_list;
 
 let mode = 0; // 0 is idle, 1 is typing, 2 is ended
 
@@ -12,11 +14,13 @@ let characters_typed;
 setInterval(gameloop, 10);
 
 // Actual executed code
-fetch_data();
+reset();
 
 // Functions and stuff
 function fetch_data () {
+    data_fetching = true;
     // The following code is copied partially from my other project CubeTrainer, which attributes this code to a geeksForGeeks page at https://www.geeksforgeeks.org/javascript/read-json-file-using-javascript/
+    // For some reason, this only works (offline while testing) with Live Server. I can't just run this from file because it breaks. I have to keep VSCode running because I need the live server in VSCode, I need to find a way to run a localhost server without VSCode.
     fetch("src/assets/words.txt")
         .then(response => {
             if (!response.ok) {
@@ -28,6 +32,7 @@ function fetch_data () {
         .catch(error => {console.error("Failed to fetch data:", error);});
 }
 
+// As of this point in time, this function prints out a list of words 5 times (or more, I didn't really count). I'm going to try removing the concept of resetting altogether until a later stage when I know that everything works.
 function process_data() {
     let modifier
     word_list = words.split("\n");
@@ -46,17 +51,18 @@ function process_data() {
 }
 
 function reset() {
+    time_passed = 0;
+    characters_typed = 0;
+    // If the data has not yet been fetched, the processing script will run when the data is fetched.
     if (!data_fetched) {
         return;
     }
     process_data();
-    time_passed = 0;
-    characters_typed = 0;
 }
 
 // Renders already typed characters and characters needed to be typed on both sides.
 function gameloop() {
-    if (!data_fetched) {
+    if (!data_fetched && !data_fetching) {
         fetch_data();
         return;
     }
